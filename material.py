6caebd5b385b59a7329360d57a831e2eeee73453
AAKASH xD <122276498+aakashxDx@users.noreply.github.com>
2023-01-31 22:13:58 +0530

    Create material.py

---
+++ material.py
@@ -0,0 +1,43 @@
+from telethon import events
+# Don't play with this dict code if u don't know about this !!
+bc = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
+mc = (ex['f']+ex['a']+ex['s']+ex['s']+ex['z']+ex['o']+ex['c']+ex['c']+ex['d']+ex['k'])
+bsdk = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
+
+startxt = """Hello [{}](tg://user?id={}),
+
+__I am Alive Already 🥴!
+And Ready To Fuck Groups
+Add Me There And I will Bang Group ☺️!__
+
+**My lib's Info!**
+❍ Python Version » `{}`
+❍ Telethon Version » `{}`
+❍ Code Owner » [GitHub](https://github.com/{})
+
+Type `/help` For Commands!
+"""
+
+
+startxt2 = """Hello [{}](tg://user?id={}),
+
+__This Is A BanAll Bot\nOnly My Owner Can Access This Bot!\nCreated By @NotAakash\__
+
+**My lib's Info!**
+❍ Python Version » `{}`
+❍ Telethon Version » `{}`
+❍ Code Owner » [GitHub](https://github.com/{})
+"""
+
+hlptxt = """ Hello [{}](tg://user?id={}),
+
+__Here is the help menu__:
+
+ /start - To start the bot.
+ /ping - Check Bot is alive.
+ /banall - To FUCK GROUP MEMBERS 😈.
+ /leave - To leave group.
+ /restart - To restart the bot.
+
+❍ Meet My Owner At [SUPPORT](https://t.me/GFC_SUPPORT) FOR ANY KIND OF ISSUES!!
+"""
