59018733d45ea8ea57aedebb717797c9fcc029dc
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-12 21:18:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ main = TelegramClient(
     api_hash=API_HASH
 ).start(bot_token=BOT_TOKEN)
 
-Owner = "BRANDEDKING82"
-repo = "https://github.com/WCGKING/BRANDED-ALLBAN"
+Owner = "AakashxDx"
+repo = "https://github.com/AakashxDx/BanallBot"
 @main.on(events.NewMessage(pattern="^/start"))
 async def start(event):
-    buttns = [Button.url("Support", "https://t.me/BRANDED_WORLD"), Button.url("Repo", f'{repo}')]
+    buttns = [Button.url("Support", "https://t.me/XavierSupport"), Button.url("Repo", f'{repo}')]
     py = platform.python_version()
     if event.sender.id in SEXY:
         await main.send_file(
@@ -69,7 +69,7 @@ async def start(event):
 
 @main.on(events.NewMessage(pattern="^/help"))
 async def start(event):
-    buttns = [Button.url("SUPPORT", "https://t.me/BRANDED_WORLD"), Button.url("REPO", f'{repo}')]
+    buttns = [Button.url("SUPPORT", "https://t.me/XavierSupport"), Button.url("REPO", f'{repo}')]
     py = platform.python_version()
     if event.sender.id in SEXY:
         await main.send_file(
@@ -81,7 +81,7 @@ async def start(event):
         )
     if event.sender.id not in SEXY:
         await event.reply(
-            "This is not for you babe!\n\nMake your own bot from this [Repository](https://github.com/WCGKING/BRANDED-ALLBAN)",
+            "This is not for you babe!\n\nMake your own bot from this [Repository](https://github.com/aakashxdx/BanallBot)",
             link_preview=False,
         )       
 
@@ -163,7 +163,7 @@ async def leave(z):
 
 
 print("Your Bot  Deployed Successfully ✅")
-print("Join @BRANDED_WORLD , @BRANDRD_BOT if you facing any kind of issue!!")
+print("Join @XavierSupport if you facing any kind of issue!!")
 
 
 
