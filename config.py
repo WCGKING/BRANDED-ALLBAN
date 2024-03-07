4e5969928beaec62b6aaeafe5484a0ce80f55450
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-03-07 11:28:31 +0530

    ʙʀᴀɴᴅᴇᴅ-ᴀʟʟʙᴀɴ

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=24509589
-    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    TOKEN="6572403691:AAFV-3PpCb7N06uqJC2mCdK4QzW6VtV5CeE"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
-    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=6796545941
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    API_ID=22981583
+    API_HASH="c02ca0834d5d92ffb31fea2b50060195"
+    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
+    START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
+    BOT_ID=7156025226
+    BOT_USERNAME="SOMEONE47BOT"
+    BOT_NAME="SOMEONE47BOT"
