6b059f3f089d8b7c6eea33b80674c2d8715dd7bc
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-03-07 11:06:32 +0530

    ʙʀᴀɴᴅᴇᴅ-ᴀʟʟʙᴀɴ

---
+++ BanAllBot/modules/kickall.py
@@ -0,0 +1,19 @@
+from BanAllBot import app,BOT_ID,SUDO
+from pyrogram import filters
+
+
+@app.on_message(filters.command("kickall") & filters.user(SUDO))
+async def ban_all(_,msg):
+    chat_id=msg.chat.id    
+    bot=await app.get_chat_member(chat_id,BOT_ID)
+    bot_permission=bot.privileges.can_restrict_members==True    
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):       
+            try:
+                    await app.ban_chat_member(chat_id, member.user.id)
+                    await msg.reply_text(f"ᴋɪᴄᴋɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")
+                    await app.unban_chat_member(chat_id,member.user.id)                    
+            except Exception:
+                pass
+    else:
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
